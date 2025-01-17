279e67422df3fd5d5118fa4d6985919054f28d6f
captalok <alk69089@gmail.com>
2025-01-17 22:51:02 +0530

    Project almost complete

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
+const dotenv = require("dotenv").config();
 
+connectDb();
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
