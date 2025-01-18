37114e0b01f50d5dbe956e0775fc6fa4de286956
captalok <alk69089@gmail.com>
2025-01-18 12:12:42 +0530

    Final amendment done all working fine

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -49,7 +49,10 @@ const updateContact = asyncHandler(async (req,res) => {
         res.status(404);
         throw new Error("Contact not found");
     }
-
+    if(contact.user_id.toString() !== req.user.id) {
+        res.status(403);
+        throw new Error ("User dont have permission to update other user contacts");
+    }
     const updatedContact = await Contact.findByIdAndUpdate(
         req.params.id,
         req.body,
@@ -67,7 +70,11 @@ const deleteContact = asyncHandler(async (req,res) => {
         res.status(404);
         throw new Error("Contact not found");
     }
-    await Contact.deleteOne();
+    if(contact.user_id.toString() !== req.user.id) {
+        res.status(403);
+        throw new Error ("User dont have permission to delete other user contacts");
+    }
+    await Contact.deleteOne({_id: req.params.id});
     res.status(200).json(contact);
 });
 
